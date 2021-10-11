8ab3888f41d08309312c32ed64fab1722f496e71
Nathan Devlin <ndevlin@seas.upenn.edu>
2021-10-11 17:42:54 -0400

    Added photon networking code

--- PaintSplat/Assets/Scripts/MobileNetwork.cs
+++ PaintSplat/Assets/Scripts/MobileNetwork.cs
@@ -9,10 +9,22 @@ public class MobileNetwork : Photon.PunBehaviour
         GUILayout.Label("           " + PhotonNetwork.connectionStateDetailed.ToString());
     }
 
-    // TODO-2.a: 
-    // Copy and paste the Start() and OnJoinedLobby() methods from MobileNetwork_Cube.cs
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        roomName = "testing";
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
 
-	public override void OnJoinedRoom()
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRoom(roomName);
+        base.OnJoinedLobby();
+    }
+
+
+    public override void OnJoinedRoom()
 	{
 		GetComponent<MobileShooter>().Activate();
 		base.OnJoinedRoom ();
